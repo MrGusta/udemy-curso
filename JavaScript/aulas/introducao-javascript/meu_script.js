73e1e4bb7494ba2f4a529395207a6404343fc39b
MrGusta <gusta1221oliveira@gmail.com>
2022-10-18 10:13:36 -0300

    Primeiro código JS

---
+++ JavaScript/aulas/introducao-javascript/meu_script.js
@@ -0,0 +1,6 @@
+//INTRODUÇÃO AO JAVASCRIPT
+alert ('Olá! Seja bem-vindo ao curso!'); 
+
+/* Selecione um elemento no DOM (documento)
+   Atualizar o valor desse elemento com uma string */
+document.getElementById('nome').value = 'Oi'
