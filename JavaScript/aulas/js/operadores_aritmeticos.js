e1a72ffaeb1093c4b5d34d9633415489cc9f43cf
MrGusta <gusta1221oliveira@gmail.com>
2022-12-11 13:56:18 -0300

    código operadores aritmeticos

---
+++ JavaScript/aulas/js/operadores_aritmeticos.js
@@ -0,0 +1,17 @@
+var num1 = prompt('Digite o valor de num1')
+var num2 = prompt('Digite o valor de num2')
+
+num1 = parseInt(num1)
+num2 = parseInt(num2)
+
+document.write('A soma entre ' + num1 + ' e ' + num2 +' é: ' + (num1 + num2) + ' <br>')
+document.write('A subtração entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 - num2) + ' <br>')
+document.write('A multiplação entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 * num2) + ' <br>')
+document.write('A divisão entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 / num2) + ' <br>')
+document.write('O modulo entre ' + num1 + ' e ' + num2 + ' é: ' + (num1	% num2) + ' <br>')
+
+
+document.write('O incremento de ' + num1 + ' é: ' + (num1++) + ' <br>')
+document.write((num1) + ' <br>')
+document.write('O decremento de ' + num2 + ' é: ' + (num2--) + ' <br>')
+document.write((num2) + ' <br>')
